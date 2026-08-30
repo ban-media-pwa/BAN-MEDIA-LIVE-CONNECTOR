ec98c542f40cbad8c811ea4171da10eeb4ff4919
ban-media-pwa <bm.musicvn@gmail.com>
2026-08-30 14:36:39 +0700

    Delete BAN-MEDIA-LIVEOS-prototype directory

--- BAN-MEDIA-LIVEOS-prototype/src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { ManagerApp } from "@/components/manager-app";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <ManagerApp />;
-}
